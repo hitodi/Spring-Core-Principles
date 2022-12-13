c2b7588d5dc921127ae6fcee97922483722f249a
hitodi <73509981+hitodi@users.noreply.github.com>
2022-12-13 20:25:41 +0900

    Bean lifecycle callback

---
+++ src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -0,0 +1,63 @@
+package hello.core.lifeCycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient { // implements InitializingBean, DisposableBean {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url  = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + " message = " + message);
+    }
+
+    public void disConnect() {
+        System.out.println("close");
+    }
+
+/*
+    초기화, 소멸 인터페이스
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disConnect();
+    }
+*/
+
+    @PostConstruct
+    public void init() {
+    System.out.println("NetworkClient.init");
+    connect();
+    call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disConnect();
+    }
+}
